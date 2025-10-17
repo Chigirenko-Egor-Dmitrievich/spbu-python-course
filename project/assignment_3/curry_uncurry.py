8a9bc20455722506fbf33b7b081ae32a506a03d1
Chigirenko Egor <st128240@student.spbu.ru>
2025-10-17 17:56:31 +0300

    more than half of the task#3 is done

---
+++ project/assignment_3/curry_uncurry.py
@@ -0,0 +1,50 @@
+import functools as ft
+from typing import Callable, Any
+
+
+def curry_explicit(func: Callable[..., Any], arity: int) -> Callable[..., Any]:
+
+    if arity < 0:
+        raise ValueError("Arity must be a non-negative number")
+
+    if arity == 0:
+        return func
+
+    @ft.wraps(func)
+    def curried_implicit(arg: Any, /) -> Any:
+
+        if arity == 1:
+            return func(arg)
+        return curry_explicit(lambda *args: func(arg, *args), arity - 1)
+
+    return curried_implicit
+
+
+def uncurry_explicit(func: Callable[..., Any], arity: int) -> Callable[..., Any]:
+
+    if arity < 0:
+        raise ValueError("Arity must be a non-negative number")
+
+    @ft.wraps(func)
+    def uncurried_implicit(*args: Any) -> Any:
+
+        if len(args) != arity:
+            first_word = "argument" if arity == 1 else "arguments"
+            second_word = "argument is" if arity == 1 else "arguments are"
+            raise ValueError(
+                f"The curried function expects {arity} "
+                + f"{first_word}, but {len(args)} "
+                + f"{second_word} given"
+            )
+
+        original_function = func
+
+        if arity == 0:
+            original_function = original_function()
+
+        for arg in args:
+            original_function = original_function(arg)
+
+        return original_function
+
+    return uncurried_implicit
---
+++ project/assignment_3/decorators.py
@@ -0,0 +1,136 @@
+import functools as ft
+import inspect
+import copy
+from collections import OrderedDict
+from typing import Any, Callable, Optional
+
+
+def cache_function(
+    _func: Optional[Callable] = None, *, limit: int = 0
+) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+
+        if limit <= 0:
+            return func
+
+        cache = OrderedDict()
+
+        @ft.wraps(func)
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            try:
+                check = hash(args)
+                key = (args, frozenset(kwargs.items()))
+            except TypeError:
+                hashable_args = list(args)
+                for i in range(len(hashable_args)):
+                    match i:
+                        case _ if type(hashable_args[i]) is list:
+                            hashable_args[i] = tuple(hashable_args[i])
+                        case _ if type(hashable_args[i]) is dict:
+                            hashable_args[i] = tuple(hashable_args[i].items())
+                        case _ if type(hashable_args[i]) is set:
+                            hashable_args[i] = frozenset(hashable_args[i])
+                        case _ if type(hashable_args[i]) is bytearray:
+                            hashable_args[i] = tuple(hashable_args[i])
+                hashable_args = tuple(hashable_args)
+                key = (hashable_args, frozenset(kwargs.items()))
+
+            if key in cache:
+                cache.move_to_end(key)
+                print(f"Returning {cache[key]} from cache")
+                return cache[key]
+
+            result = func(*args, **kwargs)
+
+            if len(cache) >= limit:
+                temp = cache.popitem(last=False)
+                print(f"Removing the oldest element: {temp}")
+
+            cache[key] = result
+            print(f"Adding new element: {key} ---> {result}")
+
+            return result
+
+        return inner
+
+    if callable(_func):
+        return decorator(_func)
+
+    return decorator
+
+
+class Evaluated:
+    def __init__(self, func: Callable[..., Any]) -> None:
+        self.func = func
+
+
+class Isolated:
+    pass
+
+
+def smart_args(
+    _func: Optional[Callable] = None, *, allow_pos_args: bool = True
+) -> Callable[..., Any]:
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        spec = inspect.getfullargspec(func)
+
+        @ft.wraps(func)
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            def_kwargs = spec.kwonlydefaults
+            if def_kwargs:
+                for kw in def_kwargs:
+
+                    if (
+                        type(def_kwargs[kw]) == Evaluated
+                        and def_kwargs[kw].func == Isolated
+                    ):
+                        assert (
+                            False
+                        ), "It is forbidden to combine Evaluated and Isolated"
+
+                    if type(def_kwargs[kw]) == Evaluated and kw not in kwargs:
+                        kwargs[kw] = def_kwargs[kw].func()
+                    elif type(def_kwargs[kw]) == Isolated:
+                        if kw in kwargs:
+                            kwargs[kw] = copy.deepcopy(kwargs[kw])
+                        else:
+                            raise ValueError(
+                                "A keyword argument with default value Isolated has not been passed"
+                            )
+            if allow_pos_args:
+                args_lst = list(args)
+                spec_args = list(spec.args)
+                spec_defaults = list(spec.defaults) if spec.defaults else []
+                start = len(args_lst) - (len(spec_args) - len(spec_defaults))
+
+                for i in range(start, len(spec_defaults)):
+                    if isinstance(spec_defaults[i], Evaluated) and isinstance(
+                        spec_defaults[i].func, Isolated
+                    ):
+                        assert (
+                            False
+                        ), "It is forbidden to combine Evaluated and Isolated"
+
+                    if isinstance(spec_defaults[i], Evaluated):
+                        args_lst.append(spec_defaults[i].func())
+                    elif isinstance(spec_defaults[i], Isolated):
+                        raise ValueError(
+                            "A positional argument with default value Isolated has not been passed"
+                        )
+                    else:
+                        args_lst.append(spec_defaults[i])
+
+                delta = len(spec_args) - len(spec_defaults)
+                for i in range(start):
+                    if isinstance(spec_defaults[i], Isolated):
+                        args_lst[delta + i] = copy.deepcopy(args_lst[delta + i])
+                args = tuple(args_lst)
+
+            return func(*args, **kwargs)
+
+        return inner
+
+    if callable(_func):
+        return decorator(_func)
+
+    return decorator
